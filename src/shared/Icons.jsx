98e214d476f71603cfebdc50b376f374462ba146
Luís Clício <cliciosa14@gmail.com>
2022-01-06 12:14:42 -0300

    feat: centers the icons that are used in the app

---
+++ src/shared/Icons.jsx
@@ -0,0 +1,9 @@
+import {
+  FiPlus as PlusIcon,
+  FiCircle as CircleIcon,
+  FiCheckCircle as CheckCircleIcon,
+  FiTrash as TrashIcon,
+  FiX as CloseIcon,
+} from 'react-icons/fi';
+
+export { PlusIcon, CircleIcon, CheckCircleIcon, TrashIcon, CloseIcon };
